e751d009f228885dd24ff46ab27201f5f36de08e
Witold Olszyk <witoldolszyk@gmail.com>
2018-03-25 01:57:19 +0100

    add google login

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var app = express();
+
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.use('/static', express.static("assets"));
+
+app.get('/', function(req, res) {
+  res.render('oknologowania',
+    {
+      user: { name: "Johnny", age: "20" }
+    }
+  );
+});
+
+app.get('/auth/google', function(req, res) {
+  res.render('zalogowany');
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('404. Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
